4bc5733de19eebd3384f91d78593682527d266ae
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-06-16 18:34:49 +0300

    added main screen

--- app/src/main/java/com/example/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesEvent.kt
+++ app/src/main/java/com/example/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesEvent.kt
@@ -7,6 +7,6 @@ sealed class NotesEvent {
     data class Order(val noteOrder: NoteOrder): NotesEvent()
     data class DeleteNote(val note: Note): NotesEvent()
     object RestoreNote: NotesEvent()
-    object ToggleOrderSelection: NotesEvent()
+    object ToggleOrderSection: NotesEvent()
 
 }
