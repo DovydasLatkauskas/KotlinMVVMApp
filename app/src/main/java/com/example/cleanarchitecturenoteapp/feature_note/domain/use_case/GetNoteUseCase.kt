e6883e2096c8b26d04cef380918ca0f6f879706c
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-06-16 18:58:03 +0300

    added AddEditNoteViewModel

--- app/src/main/java/com/example/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNoteUseCase.kt
+++ app/src/main/java/com/example/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNoteUseCase.kt
@@ -1,4 +1,12 @@
 package com.example.cleanarchitecturenoteapp.feature_note.domain.use_case
 
-class GetNoteUseCase {
+import com.example.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.example.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+
+class GetNoteUseCase(
+    private val repository: NoteRepository
+) {
+    suspend operator fun invoke(id: Int): Note? {
+        return repository.getNoteById(id)
+    }
 }
